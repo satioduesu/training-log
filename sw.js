5fb305cd78ac62035f3766734af47c64102921c0
satioduesu <syuto.red@gmail.com>
2026-07-24 11:42:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+/* 筋トレ記録 PWA - Service Worker
+   アプリ本体をキャッシュしてオフラインでも起動できるようにする。 */
+const CACHE = 'kintore-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  // 同一オリジンのGETのみキャッシュ対象。同期(script.google.com)などはそのまま通す。
+  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
